56d109d25af0beaa2b66ad3d8c460665ed1cc3c7
Tinokil <125847246+Tinokil@users.noreply.github.com>
2024-07-19 13:39:51 +0300

    Upload __init__.py

---
+++ tinLogging/__init__.py
@@ -0,0 +1,10 @@
+"""
+:authors: tinokil
+:license: MIT License, see License file
+
+:copyright: (c) 2024 tinokil
+"""
+
+from .logging import Logger, ConsoleLogger
+
+__all__ = ["Logger", "ConsoleLogger"]
